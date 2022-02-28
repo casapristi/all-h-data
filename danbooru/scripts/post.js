93ee0c1c35d1556e5cff2e64982b9d6f85c405dd
casapristi <64585816+casapristi@users.noreply.github.com>
2022-02-28 19:02:11 +0100

    Add files via upload

---
+++ danbooru/scripts/post.js
@@ -0,0 +1,207 @@
+var search={};
+window.location.search.slice(1).split("&").forEach(value=>search[value.split("=")[0]]=value.split("=")[1]);
+function toggleSidebar(){
+  const sidebar=document.getElementById("sidebar");
+  const hamburger=document.getElementById("hamburger-content");
+  if(sidebar.classList.contains("open")){
+    sidebar.classList.remove("open");
+    hamburger.classList.remove("cross");
+    sidebar.classList.add("close");
+    hamburger.classList.add("burger");
+  }else{
+    sidebar.classList.add("open");
+    hamburger.classList.add("cross");
+    sidebar.classList.remove("close");
+    hamburger.classList.remove("burger");
+  };
+};
+function category(cat){
+  if(cat==0)return"thing";
+  if(cat==1)return"user";
+  if(cat==3)return"lyc";
+  if(cat==4)return"prot";
+  if(cat==5)return"type";
+  return cat;
+};
+function rating(r){
+  if(r=="s")return"Safe";
+  if(r=="q")return"Questionnable";
+  if(r=="e")return"Explicit";
+  return"Unknown";
+};
+var imageId;
+window.onload=()=>{
+  fetch(`https://danbooru.donmai.us/posts/${search.id}.json`)
+  .then(res=>res.json())
+  .then(data=>{
+    imageId=data.id;
+    const tags=data.tag_string_general.split(" ");
+    const artists=data.tag_string_artist.split(" ");
+    const characters=data.tag_string_character.split(" ");
+    const copyrights=data.tag_string_copyright.split(" ");
+    const meta=data.tag_string_meta.split(" ");
+    if(data.file_ext=="mp4"){
+      document.getElementById("file").innerHTML=`<video alt="post" id="image" class="image" src="${data.file_url}" controls autoplay loop></video>`;
+    }else{
+      document.getElementById("file").innerHTML=`<img alt="post" id="image" class="image" src="${data.file_url}">`;
+    };
+    document.getElementById("rating").innerHTML=rating(data.rating);
+    document.getElementById("size").innerHTML=`${data.image_width} x ${data.image_height}`;
+    document.getElementById("id").innerHTML=data.id;
+    document.getElementById("created-at").innerHTML=new Date(data.created_at).toDateString();
+    document.getElementById("fav-count").innerHTML=data.fav_count.toLocaleString();
+    document.getElementById("score").innerHTML=`${data.score.toLocaleString()} (<i class="fas fa-caret-up"></i> ${data.up_score.toLocaleString()} / <i class="fas fa-caret-down"></i> ${(Math.abs(data.down_score)).toLocaleString()})`;
+    fetch(`https://danbooru.donmai.us/users/${data.uploader_id}.json`)
+    .then(res=>res.json())
+    .then(user=>{
+      document.getElementById("uploader").innerHTML=user.name;
+    });
+    document.getElementById("global").innerHTML=tags.map(tag=>{
+      return`<a class="tag-obj" onclick="setTag('${tag}')"><p class="tag-name thing">${tag}</p></a>`
+    }).join("");
+    document.getElementById("artists").innerHTML=artists.map(tag=>{
+      return`<a class="tag-obj" onclick="setTag('${tag}')"><p class="tag-name user">${tag}</p></a>`
+    }).join("");
+    document.getElementById("characters").innerHTML=characters.map(tag=>{
+      return`<a class="tag-obj" onclick="setTag('${tag}')"><p class="tag-name prot">${tag}</p></a>`
+    }).join("");
+    document.getElementById("copyrights").innerHTML=copyrights.map(tag=>{
+      return`<a class="tag-obj" onclick="setTag('${tag}')"><p class="tag-name lyc">${tag}</p></a>`
+    }).join("");
+    document.getElementById("meta").innerHTML=meta.map(tag=>{
+      return`<a class="tag-obj" onclick="setTag('${tag}')"><p class="tag-name type">${tag}</p></a>`
+    }).join("");
+    fetch(`https://danbooru.donmai.us/recommended_posts.json?search[post_id]=${data.id}&limit=20`)
+    .then(res=>res.json())
+    .then(posts=>{
+      if(!posts.length){
+        document.getElementById("recommendations").innerHTML="No posts found...";
+      }else{
+        document.getElementById("recommendations").innerHTML=posts.map(({post})=>`<img src="${post.preview_file_url}" alt="${post.id}" onclick="setImage(${post.id})">`).join("");
+      };
+    });
+    fetch(`https://danbooru.donmai.us/artist_commentary_versions.json?search[post_id]=${data.id}`)
+    .then(res=>res.json())
+    .then(commentary=>{
+      if(commentary.length){
+        var content=commentary[0].original_description.replace(/(\[\/?t(r|d|h|able|body)\])\n/g,(_,b)=>b).replace(/"(.*?[^""])":\[(.*?[^\[\]])\]/g,(_,label,link)=>{
+          if(link.startsWith("/"))return`<b>${label}</b>`;
+          return`<a href="${link}">${label}</a>`;
+        });
+        content=content.replace(/\r/g,"").replace(/\n/g,"<br>").replace(/\[/g,"<").replace(/\]/g,">");
+        document.getElementById("commentary").innerHTML=content;
+      }else{
+        document.getElementById("commentary").innerHTML="No commentary...";
+      };
+    });
+  });
+  fetch(`https://danbooru.donmai.us/comments.json?search[post_id]=${search.id}&search[order]=score`)
+  .then(res=>res.json())
+  .then(data=>{
+    document.getElementById("comments").innerHTML=data.map(comment=>{
+      fetch(`https://danbooru.donmai.us/users/${comment.updater_id}.json`)
+      .then(res=>res.json())
+      .then(user=>{
+        document.getElementById(`user-${comment.id}`).innerHTML=user.name;
+      });
+      comment.body=comment.body.replace(/(\[\/?t(r|d|h|able|body)\])\n/g,(_,b)=>b).replace(/"(.*?[^""])":\[(.*?[^\[\]])\]/g,(_,label,link)=>{
+        if(link.startsWith("/"))return`<b>${label}</b>`;
+        return`<a href="${link}">${label}</a>`;
+      });
+      const quote=comment.body.split(/\[quote\]/)?.[1]?.split(/\[\/quote\]/)[0];
+      var content=comment.body;
+      if(quote!=undefined)content=comment.body.split(/\[\/quote\]/)[1];
+      content=content.replace(/\r/g,"").replace(/\n/g,"<br>").replace(/\[/g,"<").replace(/\]/g,">");
+      return`<div class="comment">
+        <div class="comment-content">
+          ${quote?`<p class="quote">${quote}</p>`:""}
+          <div class="comment-info">
+            <p class="comment-user" id="user-${comment.id}">Unknown</p>
+            <p class="comment-date">${new Date(comment.updated_at).toDateString()}</p>
+          </div>
+          <p class="comment-body">${content}</p>
+        </div>
+        <p class="comment-score">${comment.score}</p>
+      </div>`
+    }).join("");
+  });
+};
+function setImage(postId){
+  search.id=postId;
+  delete search[""];
+  window.location.search="?"+Object.entries(search).map(e=>`${e[0]}=${e[1]}`).join("&");
+};
+async function downloadImage(){
+  const file=document.getElementById("image").src;
+  const fileName=file.split("/")[file.split("/").length-1];
+  const image=await fetch(file);
+  const imageBlog=await image.blob();
+  const imageURL=URL.createObjectURL(imageBlog);
+  const link=document.createElement("a");
+  link.href=imageURL;
+  link.download=imageId+"-"+fileName;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+};
+function randomPost(){
+  fetch("https://danbooru.donmai.us/posts/random.json")
+  .then(res=>res.json())
+  .then(data=>{
+    search.id=data.id;
+    delete search[""];
+    window.location.search="?"+Object.entries(search).map(e=>`${e[0]}=${e[1]}`).join("&");
+  });
+};
+function operatePost(num){
+  search.id=Number(search.id)+num;
+  delete search[""];
+  window.location.search="?"+Object.entries(search).map(e=>`${e[0]}=${e[1]}`).join("&");
+};
+function predict(input){
+  const predictions=document.getElementById("predictions");
+  const value=input.value.split(/ +/)[input.value.split(/ +/).length-1];
+  if(!input.value.length||!value.length)return predictions.innerHTML="";
+  if(value.startsWith("pool:")){
+    fetch(`https://danbooru.donmai.us/pools.json?search[name_matches]=${encodeURIComponent(value.slice(5))}*&search[order]=count&search[hide_empty]=t`)
+    .then(res=>res.json())
+    .then(displayPredictions);
+  }else{
+    fetch(`https://danbooru.donmai.us/tags.json?search[name_matches]=${encodeURIComponent(value)}*&search[order]=count&search[hide_empty]=t`)
+    .then(res=>res.json())
+    .then(displayPredictions);
+  };
+};
+function displayPredictions(data){
+  predictions.innerHTML=data.slice(0,10).map(t=>{
+    if(t.name.length>20)t.name=t.name.slice(0,17)+" ...";
+    return`
+    <div class="prediction" onclick="complete('${t.post_ids?`pool:${t.id}`:t.name}')"><p class="tag-name ${category(t.category)}">${t.name}</p><p class="post-count">${t.post_count.toLocaleString()}</p></div>
+    `;
+  }).join("");
+};
+function complete(prediction){
+  const searchBar=document.getElementById("search-bar");
+  const values=searchBar.value.trim().split(/ +/);
+  const last=values.splice(values.length-1,1);
+  searchBar.value=searchBar.value.replace(last,prediction)+" ";
+  searchBar.focus();
+  predict(searchBar);
+};
+window.onkeydown=({code})=>{
+  if(code=="Enter"&&document.activeElement.id=="search-bar"){
+    sendTags(document.activeElement);
+  };
+};
+function setTag(tag){
+  const searchBar=document.getElementById("search-bar");
+  searchBar.value=tag;
+  sendTags(searchBar);
+};
+function sendTags(input){
+  const tags=input.value.trim().split(/ +/).map(t=>encodeURIComponent(t)).join("+");
+  search.tags=tags;
+  delete search[""];
+  delete search.id;
+  window.location.href="../danbooru.html?"+Object.entries(search).map(e=>`${e[0]}=${e[1]}`).join("&");
+};
---
+++ scripts/danbooru.js
@@ -0,0 +1,145 @@
+var search={};
+window.location.search.slice(1).split("&").forEach(value=>search[value.split("=")[0]]=value.split("=")[1]);
+function toggleSidebar(){
+  const sidebar=document.getElementById("sidebar");
+  const hamburger=document.getElementById("hamburger-content");
+  if(sidebar.classList.contains("open")){
+    sidebar.classList.remove("open");
+    hamburger.classList.remove("cross");
+    sidebar.classList.add("close");
+    hamburger.classList.add("burger");
+  }else{
+    sidebar.classList.add("open");
+    hamburger.classList.add("cross");
+    sidebar.classList.remove("close");
+    hamburger.classList.remove("burger");
+  };
+};
+
+function category(cat){
+  if(cat==0)return"thing";
+  if(cat==1)return"user";
+  if(cat==3)return"lyc";
+  if(cat==4)return"prot";
+  if(cat==5)return"type";
+  return cat;
+};
+var tags=search.tags?.length?`&tags=${search.tags}`:"";
+const pages=[];
+window.onload=()=>{
+  document.getElementById("search-bar").value=decodeURIComponent(search.tags?.replace(/\+/g," ")||"");
+  var page=search.page?Number(search.page):1;
+  var count=0;
+  const el=5;
+  var min=page-Math.floor(el/2);
+  if(min<1)min=1;
+  var max=min+el;
+  for(let i=min;i<max;i++){
+    if(i>1e3||i<1)continue;
+    count++;
+    fetch(`https://danbooru.donmai.us/posts.json?page=${i}&limit=75${tags}`)
+    .then(res=>res.json())
+    .then(data=>{
+      if(data.length)pages.push(i);
+      else count--;
+      if(count==pages.length){
+        const minNum=Math.min(...pages);
+        const eq=el-pages.length;
+        for(let j=0;j<eq;j++){
+          if(minNum-j-1<1)break;
+          pages.unshift(minNum-j-1);
+        };
+        document.getElementById("pages").innerHTML=pages.sort((a,b)=>a-b).map(p=>`<a class="page${page==p?" current-page":""}" onclick="setPage(${p})">${p}</a>`).join("");
+      };
+    });
+  };
+  fetch(`https://danbooru.donmai.us/posts.json?page=${page}&limit=50${tags}`)
+  .then(res=>res.json())
+  .then(data=>{
+    const tags={};
+    data.forEach(image=>{
+      image.tag_string.split(" ").forEach(t=>{
+        if(!tags[t])tags[t]=1;
+        else tags[t]++;
+      });
+    });
+    const tagsElem=document.getElementById("main-tags");
+    const images=document.getElementById("images");
+    tagsElem.innerHTML=Object.entries(tags).sort((a,b)=>b[1]-a[1]).slice(0,15).map(e=>`<a class="tag" onclick="setTag('${e[0]}')"><p class="tag-name thing">${e[0]}</p><p class="tag-usage">${e[1]}</p></a>`).join("");
+    images.innerHTML=data.map(e=>{
+      if(!e?.preview_file_url)return"";
+      return`<img src="${e.preview_file_url}" alt="${e.id}" class="image" onclick="window.location.href='danbooru/post.html?id=${e.id}'">`;
+    }).join("");
+  });
+};
+function setPage(page){
+  if(!pages.includes(page))return;
+  search.page=page;
+  delete search[""];
+  if(search.tags=="")delete search.tags;
+  window.location.search="?"+Object.entries(search).map(e=>`${e[0]}=${e[1]}`).join("&");
+};
+function sendTags(input){
+  const tags=input.value.trim().split(/ +/).map(t=>encodeURIComponent(t)).join("+");
+  search.tags=tags;
+  delete search[""];
+  if(search.tags=="")delete search.tags;
+  if(search.page)delete search.page;
+  window.location.search="?"+Object.entries(search).map(e=>`${e[0]}=${e[1]}`).join("&");
+};
+function predict(input){
+  const predictions=document.getElementById("predictions");
+  const value=input.value.split(/ +/)[input.value.split(/ +/).length-1];
+  if(!input.value.length||!value.length)return predictions.innerHTML="";
+  if(value.startsWith("pool:")){
+    fetch(`https://danbooru.donmai.us/pools.json?search[name_matches]=${encodeURIComponent(value.slice(5))}*&search[order]=count&search[hide_empty]=t`)
+    .then(res=>res.json())
+    .then(displayPredictions);
+  }else{
+    fetch(`https://danbooru.donmai.us/tags.json?search[name_matches]=${encodeURIComponent(value)}*&search[order]=count&search[hide_empty]=t`)
+    .then(res=>res.json())
+    .then(displayPredictions);
+  };
+};
+function displayPredictions(data){
+  predictions.innerHTML=data.slice(0,10).map(t=>{
+    if(t.name.length>20)t.name=t.name.slice(0,17)+" ...";
+    return`
+    <div class="prediction" onclick="complete('${t.post_ids?`pool:${t.id}`:t.name}')"><p class="tag-name ${category(t.category)}">${t.name}</p><p class="post-count">${t.post_count.toLocaleString()}</p></div>
+    `;
+  }).join("");
+};
+function setTag(tag){
+  const searchBar=document.getElementById("search-bar");
+  searchBar.value=tag;
+  sendTags(searchBar);
+};
+function complete(prediction){
+  const searchBar=document.getElementById("search-bar");
+  const values=searchBar.value.trim().split(/ +/);
+  const last=values.splice(values.length-1,1);
+  searchBar.value=searchBar.value.replace(last,prediction)+" ";
+  searchBar.focus();
+  predict(searchBar);
+};
+window.onkeydown=({code})=>{
+  if(code=="Enter"&&document.activeElement.id=="search-bar"){
+    sendTags(document.activeElement);
+  };
+};
+function nextPage(){
+  if(!search.page)search.page=2;
+  else search.page=Number(search.page)+1;
+  if(!pages.includes(search.page))return search.page--;
+  delete search[""];
+  if(search.tags=="")delete search.tags;
+  window.location.search="?"+Object.entries(search).map(e=>`${e[0]}=${e[1]}`).join("&");
+};
+function prevPage(){
+  if(!search.page||Number(search.page)==1)return;
+  else search.page=Number(search.page)-1;
+  delete search[""];
+  if(!search.page)return delete search.page;
+  if(search.tags=="")delete search.tags;
+  window.location.search="?"+Object.entries(search).map(e=>`${e[0]}=${e[1]}`).join("&");
+};
